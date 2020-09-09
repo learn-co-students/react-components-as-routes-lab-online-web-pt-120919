2f76bf790b4e8e6abb822bf86bfe3a3ce1058f8a
Charity Parks <tuppercharity@gmail.com>
2020-09-09 17:15:37 -0400

    Done.

--- src/components/Actors.js
+++ src/components/Actors.js
@@ -5,8 +5,34 @@ const Actors = () => {
   return (
     <div>
       {/*{code here}*/}
+      <h1>Actors Page</h1>
+      {actors.map((actor, index) => (
+        <div key={index}>
+          <h3>Name: {actor.name}</h3>
+          <p>Movies:</p>
+          <ul>
+            {actor.movies.map((movie, index) => (
+              <li key={index}>{movie}</li>
+            ))}
+          </ul>
+        </div>
+      ))}
+
     </div>
   );
 };
 
 export default Actors;
+
+
+// {directors.map((director, index) => (
+//   <div key={index}>
+//     <h3>Name: {director.name}</h3>
+//     <p>Movies:</p>
+//     <ul>
+//       {director.movies.map((movie, index) => (
+//         <li key={index}>{movie}</li>
+//       ))}
+//     </ul>
+//   </div>
+// ))}
